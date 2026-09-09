feb07b27241ec60d5ae8d644a37eac81865c95a6
tapiru <patarawadee.kunna@gmail.com>
2026-09-09 18:48:09 +0700

    Initial commit chapter4-casestudy

---
+++ chapter4-casestudy/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module se233.chapter4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.logging.log4j;
+    opens se233.chapter4 to javafx.fxml;
+    exports se233.chapter4;
+}
